6f4b1ff35b1cb788878a83f3468d49ee45fd960c
Arya Fardmanesh <aryafardmanesh.1383@gmail.com>
2024-11-28 16:13:39 -0800

    Add: basic files.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import output from './output';
+import input from './input';
+import attributes from './attributes';
+import helper from './helper';
+
 const softio = {
-	version: '@VERSION'
+	version: '@VERSION',
+	...output,
+	...input,
+	...attributes,
+	...helper
 };
 
 export { softio }
