0fcc0505abbf3bf2339de44655b676cfd6c49563
Arya Fardmanesh <aryafardmanesh.1383@gmail.com>
2024-12-28 10:48:14 -0800

    Build: the project.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import output from './output';
-import input from './input';
-import attributes from './attributes';
-import helper from './helper';
-import events from './events';
+// import output from './output';
+// import input from './input';
+// import attributes from './attributes';
+// import helper from './helper';
+// import events from './events';
 
-const softio = {
-	version: '@VERSION',
-	...output,
-	...input,
-	...attributes,
-	...helper,
-	...events
-};
+// export default {
+// 	version: '@VERSION',
+// 	...output,
+// 	...input,
+// 	...attributes,
+// 	...helper,
+// 	...events
+// };
 
-export { softio }
+export { };
