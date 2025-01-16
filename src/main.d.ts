8a55b93b88de682e96cbc59c7e07af62f8f60d15
Arya Fardmanesh <aryafardmanesh.1383@gmail.com>
2025-01-16 11:22:13 -0800

    Update: main.d.ts file and create a new architecture for softio in declare file.

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,108 +1,128 @@
-// ========== Output Methods ==========
+/**
+ * @description Installed package version.
+*/
+export declare const version: string;
 
 /**
- * @description This function concatenates the arguments
- * together with a whitespace and prints it in the output.
-**/
-export declare function write( ...message: unknown[] ): void;
+ * @description This class is for output methods.
+*/
+export declare class Out {
+	/**
+	 * @description Used to print data to the console.
+	**/
+	public static write( ...message: unknown[] ): void;
 
-/**
- * @description This function concatenates the arguments
- * with a blank space and creates a new line at the end
- * and prints it in the output.
-**/
-export declare function writeln( ...message: unknown[] ): void;
+	/**
+	 * @description Used to print data to the console.
+	 * It also creates a newline at the end of the message.
+	**/
+	public static writeln( ...message: unknown[] ): void;
 
-/**
- * @description This function is used to print a string.
- * Its approach is similar to C language. For more
- * information, refer to its documentation at the following
- * address.
- * https://github.com/AryaFardmanesh/softio/blob/main/DOCS/api.md
-**/
-export declare function printf( message: string, ...argv: unknown[] ): void;
+	/**
+	 * @description It is used to display and print data
+	 * in C style.
+	**/
+	public static printf( message: string, ...argv: unknown[] ): void;
 
-/**
- * @description This function is implemented to print an error
- * message. Its approach is similar to the 'printf' function,
- * except that you write the input method to the operating system's
- * 'stderr' instead of 'stdout'.
-**/
-export declare function error( message: string, ...argv: unknown[] ): void;
-
-// ========== Output Methods ==========
-
-
-// ========== Input Methods ==========
-
-/**
- * @description The function reads a value from the input and
- * returns it.
-**/
-export declare function input( message: string ): Promise<string>;
-
-/**
- * @description This function takes a confirmation from the
- * user and returns the result as a boolean.
-**/
-export declare function confirm( message: string ): Promise<boolean>;
-
-/**
- * @description This function takes a number from the input
- * and returns it as a Number type.
-**/
-export declare function readNumber( message: string ): Promise<number>;
-
-// ========== Input Methods ==========
-
-
-// ========== Attributes Methods ==========
-
-// TODO: The accessors(getter/setter) in this file cannot be exported
-// correctly. URL="https://github.com/AryaFardmanesh/softio/issues/15"
-
-// ========== Attributes Methods ==========
-
-
-// ========== Helper Methods ==========
-
-/**
- * @description This function will center and return the text
- * in the Word document. (This function does not print anything.)
-**/
-export declare function center( message: string ): string;
-
-/**
- * @description This function clears the current page the
- * user is viewing. 
-**/
-export declare function clear(): void;
-
-// ========== Helper Methods ==========
-
-
-// ========== Event Methods ==========
-
-declare enum eventTypes {
-	close 		= 'close'	,
-	error 		= 'error'	,
-	prefinish 	= 'prefinish'	,
-	finish 		= 'finish'	,
-	drain 		= 'drain'	,
-	data 		= 'data'	,
-	end 		= 'end'		,
-	readable 	= 'readable'	,
-	resize 		= 'resize'	,
+	/**
+	 * @description It is used to print error messages on
+	 * the console. It works similarly to the "printf" method.
+	**/
+	public static error( message: string, ...argv: unknown[] ): void;
 }
 
 /**
- * @description This method sets an event for the terminal.
-**/
-export declare function addEventListener( type: eventTypes, listener: Function ): void;
+ * @description This class is for input methods.
+*/
+export declare class In {
+	/**
+	 * @description This method is used to get data from
+	 * the input.
+	**/
+	public static input( message: string ): Promise<string>;
+
+	/**
+	 * @description Used to obtain user consent.
+	**/
+	public static confirm( message: string ): Promise<boolean>;
+
+	/**
+	 * @description It is used to get a number from the
+	 * input.
+	**/
+	public static readNumber( message: string ): Promise<number>;
+}
 
 /**
- * @description This method deletes an event that has already been set.
-**/
-export declare function removeEventListener( type: eventTypes ): void;
+ * @description This class is for attributes methods.
+*/
+export declare class Attr {
+	/**
+	 * @description This property returns the title of
+	 * the console page.
+	**/
+	public static get title(): string;
 
-// ========== Event Methods ==========
+	/**
+	 * @description This property sets the console page
+	 * title.
+	**/
+	public static set title( value: string );
+
+	/**
+	 * @description Returns the width of the console
+	 * screen. (The number of characters that fit in the
+	 * width)
+	**/
+	public static get width(): number;
+
+	/**
+	 * @description Returns the height of the console
+	 * screen. (Number of characters proportional to the
+	 * height)
+	**/
+	public static get height(): number;
+}
+
+declare type EventTypesT =
+	'close' 	|
+	'error' 	|
+	'prefinish' 	|
+	'finish' 	|
+	'drain' 	|
+	'data' 		|
+	'end' 		|
+	'readable' 	|
+	'resize'
+;
+
+/**
+ * @description This class is for event-related methods.
+*/
+export declare class Events {
+	/**
+	 * @description Sets a new event.
+	**/
+	public static addEventListener( type: EventTypesT, listener: Function ): void;
+
+	/**
+	 * @description Deletes the previously set event.
+	**/
+	public static removeEventListener( type: EventTypesT ): void;
+}
+
+/**
+ * @description This class is for utilities methods.
+*/
+export declare class Utilities {
+	/**
+	 * @description It centeralize the message in the middle
+	 * of the console screen and returns it.
+	**/
+	public static center( message: string ): string;
+
+	/**
+	 * @description Clears the console screen.
+	**/
+	public static clear(): void;
+}
