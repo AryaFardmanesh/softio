d28172185a27d5c02c5697a97f6a11b0a9240cf8
Arya Fardmanesh <aryafardmanesh.1383@gmail.com>
2025-01-17 11:40:57 -0800

    Change: utilities.ts methods structure.

---
+++ src/utilities.ts
@@ -0,0 +1,23 @@
+import typeCheck from './utils/typecheck';
+import { stdout } from './var/stdout';
+
+export default class Utilities {
+	public static center( message: string ): string {
+		typeCheck( 'center', 'string', message );
+
+		const endSpace: number = ( stdout.columns / 2 ) - ( message.length / 2 );
+		let centerMessage: string = '';
+
+		// Columns start from one.
+		for ( let i: number = 1; i <= endSpace; i++ ) {
+			centerMessage += ' ';
+		}
+		centerMessage += message;
+
+		return centerMessage;
+	}
+
+	public static clear(): void {
+		stdout.write( '\x1b[2J' );
+	}
+}
