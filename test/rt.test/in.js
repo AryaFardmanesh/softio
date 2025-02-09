588572ee1cdb8eb5a5d021fb5476778f77e089b8
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-02-09 16:56:34 +0330

    Add: rt test for password method.

--- test/rt.test/in.js
+++ test/rt.test/in.js
@@ -4,8 +4,10 @@ const { In } = require( './module' );
 	const name = await In.input( 'Enter your name: ' );
 	const age = await In.readNumber( 'Enter your age: ' );
 	const login = await In.confirm( 'Do you want to login into this app ?' );
+	const pass = await In.password( 'Enter your password: ', '*' );
 
 	console.log( 'name:', name );
 	console.log( 'age:', age );
 	console.log( 'confirm:', login );
+	console.log( 'password:', pass );
 } ) ();
