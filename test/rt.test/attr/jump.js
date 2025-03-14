2b19f01c850d9efb36b8a9f12431843f0cae1e46
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-03-14 14:23:44 +0330

    Remove: unnecessary params in jump js file in rt test.

--- test/rt.test/attr/jump.js
+++ test/rt.test/attr/jump.js
@@ -1,9 +1,9 @@
 const { Attr } = require( '../module' );
 
 Attr.move( 10, 10 );
-Attr.cursorSave( 'SCO' );
+Attr.cursorSave();
 console.log( 'Hello' );
 
 Attr.move( 20, 20 );
-Attr.cursorRestore( 'SCO' );
+Attr.cursorRestore();
 console.log( 'Hello' );
