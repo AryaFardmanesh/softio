1d06b90b48629ef4ae75df39ebb5ecf6c7ccff4f
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-03-14 14:20:31 +0330

    Change: something in move js file in rt test.

--- test/rt.test/attr/move.js
+++ test/rt.test/attr/move.js
@@ -3,8 +3,8 @@ const { Attr } = require( '../module' );
 Attr.move( 10, 20 );
 console.log( 'Move the cursor to 10x20.' );
 
-Attr.moveCol( 0 );
-console.log( 'Move the column position to 0.' );
+Attr.moveCol( 1 );
+console.log( 'Move the column position to 1.' );
 
 Attr.moveHome();
-console.log( 'Move the cursor to home (0x0).' );
+console.log( 'Move the cursor to home (1x1).' );
