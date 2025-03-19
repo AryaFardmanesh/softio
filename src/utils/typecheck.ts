2083c4ea07369f352d422acfd6f8758e1c72a5f2
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-03-19 14:20:43 +0330

    Update: typecheck method, now it can handle array of types for check data type.

--- src/utils/typecheck.ts
+++ src/utils/typecheck.ts
@@ -2,8 +2,16 @@
  * @description This function checks whether
  * the data type is correct.
 **/
-export default function typeCheck( name: string, expected: string, data: unknown ): void {
-	if ( typeof data !== expected ) {
-		throw new TypeError( `The '${ name }' method given only ${ expected } message.` );
+export default function typeCheck( name: string, expected: string | string[], data: unknown ): void | never {
+	if ( Array.isArray( expected ) ) {
+		for ( const expectedType of expected ) {
+			if ( typeof data === expectedType ) {
+				return;
+			}
+		}
+	}else if ( typeof data === expected ) {
+		return;
 	}
+
+	throw new TypeError( `The '${ name }' method given only ${ expected } message.` );
 }
