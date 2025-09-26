037d4c1ad0e9f0417e0df3c531828983a5e7084f
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-09-26 15:35:15 +0330

    Add: symbols for save attr properties.

---
+++ src/var/attrSymbols.ts
@@ -0,0 +1,3 @@
+export const _bg = Symbol('The _bg property for attribute.');
+export const _color = Symbol('The _color property for attribute.');
+export const _fontStyle = Symbol('The _fontStyle property for attribute.');
