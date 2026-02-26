39e9e7038c0f116f5d9cd715444c9831010f5e8d
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2026-02-26 17:46:50 +0330

    Optimize: color convertion.

--- src/var/ansi/color.ts
+++ src/var/ansi/color.ts
@@ -4,6 +4,46 @@ import {
 	ANSI_Background_T
 } from '../../main.d';
 
+const textColors = {
+	'black': '\x1B[30m',
+	'red': '\x1B[31m',
+	'green': '\x1B[32m',
+	'yellow': '\x1B[33m',
+	'blue': '\x1B[34m',
+	'magenta': '\x1B[35m',
+	'cyan': '\x1B[36m',
+	'white': '\x1B[37m',
+	'default': '\x1B[39m',
+	'bright-black': '\x1B[90m',
+	'bright-red': '\x1B[91m',
+	'bright-green': '\x1B[92m',
+	'bright-yellow': '\x1B[93m',
+	'bright-blue': '\x1B[94m',
+	'bright-magenta': '\x1B[95m',
+	'bright-cyan': '\x1B[96m',
+	'bright-white': '\x1B[97m'
+};
+
+const backgroundColors = {
+	'black': '\x1B[40m',
+	'red': '\x1B[41m',
+	'green': '\x1B[42m',
+	'yellow': '\x1B[43m',
+	'blue': '\x1B[44m',
+	'magenta': '\x1B[45m',
+	'cyan': '\x1B[46m',
+	'white': '\x1B[47m',
+	'default': '\x1B[49m',
+	'bright-black': '\x1B[100m',
+	'bright-red': '\x1B[101m',
+	'bright-green': '\x1B[102m',
+	'bright-yellow': '\x1B[103m',
+	'bright-blue': '\x1B[104m',
+	'bright-magenta': '\x1B[105m',
+	'bright-cyan': '\x1B[106m',
+	'bright-white': '\x1B[107m'
+};
+
 export function convertTextColorToANSI( color: ANSI_Color_T | number ): string {
 	if ( typeof color === 'number' ) {
 		if ( color > 255 || color < 0 ) {
@@ -13,44 +53,12 @@ export function convertTextColorToANSI( color: ANSI_Color_T | number ): string {
 		return `\x1B[38;5;${color}m`;
 	}
 
-	switch ( color ) {
-		case 'black':
-			return '\x1B[30m';
-		case 'red':
-			return '\x1B[31m';
-		case 'green':
-			return '\x1B[32m';
-		case 'yellow':
-			return '\x1B[33m';
-		case 'blue':
-			return '\x1B[34m';
-		case 'magenta':
-			return '\x1B[35m';
-		case 'cyan':
-			return '\x1B[36m';
-		case 'white':
-			return '\x1B[37m';
-		case 'default':
-			return '\x1B[39m';
-		case 'bright-black':
-			return '\x1B[90m';
-		case 'bright-red':
-			return '\x1B[91m';
-		case 'bright-green':
-			return '\x1B[92m';
-		case 'bright-yellow':
-			return '\x1B[93m';
-		case 'bright-blue':
-			return '\x1B[94m';
-		case 'bright-magenta':
-			return '\x1B[95m';
-		case 'bright-cyan':
-			return '\x1B[96m';
-		case 'bright-white':
-			return '\x1B[97m';
-		default:
-			throw new TypeError( `The color name '${ color }' is invalid for a text color.` );
+	const result = textColors[ color ];
+	if ( result === undefined ) {
+		throw new TypeError( `The color name '${ color }' is invalid for a text color.` );
 	}
+
+	return result;
 }
 
 export function convertTextBackgroundToANSI( color: ANSI_Background_T | number ): string {
@@ -62,44 +70,12 @@ export function convertTextBackgroundToANSI( color: ANSI_Background_T | number )
 		return makeANSI( [ '48', '5', color ] );
 	}
 
-	switch ( color ) {
-		case 'black':
-			return makeANSI( [ '40' ] );
-		case 'red':
-			return makeANSI( [ '41' ] );
-		case 'green':
-			return makeANSI( [ '42' ] );
-		case 'yellow':
-			return makeANSI( [ '44' ] );
-		case 'blue':
-			return makeANSI( [ '44' ] );
-		case 'magenta':
-			return makeANSI( [ '45' ] );
-		case 'cyan':
-			return makeANSI( [ '46' ] );
-		case 'white':
-			return makeANSI( [ '47' ] );
-		case 'default':
-			return makeANSI( [ '49' ] );
-		case 'bright-black':
-			return makeANSI( [ '100' ] );
-		case 'bright-red':
-			return makeANSI( [ '101' ] );
-		case 'bright-green':
-			return makeANSI( [ '102' ] );
-		case 'bright-yellow':
-			return makeANSI( [ '103' ] );
-		case 'bright-blue':
-			return makeANSI( [ '104' ] );
-		case 'bright-magenta':
-			return makeANSI( [ '105' ] );
-		case 'bright-cyan':
-			return makeANSI( [ '106' ] );
-		case 'bright-white':
-			return makeANSI( [ '107' ] );
-		default:
-			throw new TypeError( `The color name '${ color }' is invalid for a background color.` );
+	const result = backgroundColors[ color ];
+	if ( result === undefined ) {
+		throw new TypeError( `The color name '${ color }' is invalid for a background color.` );
 	}
+
+	return result;
 }
 
 export function isValidHex( hex: string ): boolean {
