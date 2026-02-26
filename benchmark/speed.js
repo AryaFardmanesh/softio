4c2189739cc9db0d669ea5f46ca709597eb24a95
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2026-02-26 16:12:01 +0330

    Update: bench speed js file.

--- benchmark/speed.js
+++ benchmark/speed.js
@@ -42,3 +42,23 @@ function bench(name, fn, iterations = 100000) {
 	console.log( `${name.padEnd(20)}: ${total.toFixed(2)} ms` );
 	return total;
 }
+
+/*
+History of benchmarks
+
+Version 4.0.0:
+=========== Softio Benchmark ===========
+Console.Styler.color(name): 91.75 ms
+Console.Styler.color(hex): 132.24 ms
+Console.Styler.color(rgb): 90.47 ms
+Console.Styler.color(ansi): 78.59 ms
+
+Console.Styler.background(name): 88.35 ms
+Console.Styler.background(hex): 123.77 ms
+Console.Styler.background(rgb): 89.87 ms
+Console.Styler.background(ansi): 78.50 ms
+
+Console.Styler.bold.red(text): 99.47 ms
+Console.Styler.bold.red.bgBlue(text): 126.85 ms
+========================================
+*/
