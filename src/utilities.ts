fe699591918616ba9d0d40d52bcb89dc53383b95
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2026-02-26 20:32:27 +0330

    Optimize: color and background function in utils class.

--- src/utilities.ts
+++ src/utilities.ts
@@ -5,6 +5,12 @@ import colorConvertor from './utils/colorconvertor';
 import {convertTextStyleToANSI } from './var/ansi/style';
 import silentEcho from './utils/silentecho';
 import {
+	backgroundColors,
+	convertHexToRGB,
+	hexPattern,
+	textColors
+} from './var/ansi';
+import {
 	ANSI_Style_T,
 	ColorParam_T,
 	BgColorParam_T
@@ -27,21 +33,47 @@ export default class Utils {
 	}
 
 	public static clear(): void {
-		stdout.write( '\x1b[2J' );
+		stdout.write( '\x1B[2J' );
 	}
 
 	public static reset(): string {
-		return makeANSI( [ '0' ] );
+		return '\x1B[0m';
 	}
 
 	public static color( color: ColorParam_T ): string {
-		typeCheck( 'color', [ 'string', 'number', 'object' ], color );
-		return colorConvertor( 'color', 'color', color );
+		if ( Array.isArray( color ) ) {
+			return `\x1B[38;2;${ color[0] };${ color[1] };${ color[2] }m`;
+		}else if ( typeof color === 'number' ) {
+			return `\x1B[38;5;${ color }m`;
+		}else if ( hexPattern.test( color ) ) {
+			const rgb = convertHexToRGB( color );
+			return `\x1B[38;2;${ rgb[0] };${ rgb[1] };${ rgb[2] }m`;
+		}
+
+		const result = textColors[ color ];
+		if ( result !== undefined ) {
+			return result;
+		}
+
+		return textColors.default;
 	}
 
 	public static background( color: BgColorParam_T ): string {
-		typeCheck( 'color', [ 'string', 'number', 'object' ], color );
-		return colorConvertor( 'background', 'bg', color );
+		if ( Array.isArray( color ) ) {
+			return `\x1B[48;2;${ color[0] };${ color[1] };${ color[2] }m`;
+		}else if ( typeof color === 'number' ) {
+			return `\x1B[48;5;${ color }m`;
+		}else if ( hexPattern.test( color ) ) {
+			const rgb = convertHexToRGB( color );
+			return `\x1B[48;2;${ rgb[0] };${ rgb[1] };${ rgb[2] }m`;
+		}
+
+		const result = backgroundColors[ color ];
+		if ( result !== undefined ) {
+			return result;
+		}
+
+		return textColors.default;
 	}
 
 	public static fontStyle( style: ANSI_Style_T ): string {
