2724e4283ccfd5a1c035d4322a8906d4c209ee96
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2026-02-27 09:53:58 +0330

    Add: some methods to utils class.

--- src/utilities.ts
+++ src/utilities.ts
@@ -11,7 +11,9 @@ import {
 import {
 	ANSI_Style_T,
 	ColorParam_T,
-	BgColorParam_T
+	BgColorParam_T,
+	ANSI_Color_T,
+	ANSI_Background_T
 } from './main.d';
 
 export default class Utils {
@@ -54,6 +56,28 @@ export default class Utils {
 		return textColors.default;
 	}
 
+	public static colorRGB( red: number, green: number, blue: number ): string {
+		return `\x1B[38;2;${ red };${ green };${ blue }m`;
+	}
+
+	public static colorHex( hex: string ): string {
+		const rgb = convertHexToRGB( hex );
+		return `\x1B[38;2;${ rgb[ 0 ] };${ rgb[ 1 ] };${ rgb[ 2 ] }m`;
+	}
+
+	public static colorAnsi256( color: number ): string {
+		return `\x1B[38;5;${ color }m`;
+	}
+
+	public static colorName( name: ANSI_Color_T ): string {
+		const result = textColors[ name ];
+		if ( result !== undefined ) {
+			return result;
+		}
+
+		return textColors.default;
+	}
+
 	public static background( color: BgColorParam_T ): string {
 		if ( Array.isArray( color ) ) {
 			return `\x1B[48;2;${ color[0] };${ color[1] };${ color[2] }m`;
@@ -70,6 +94,28 @@ export default class Utils {
 		}
 
 		return textColors.default;
+	}
+
+	public static backgroundRGB( red: number, green: number, blue: number ): string {
+		return `\x1B[48;2;${ red };${ green };${ blue }m`;
+	}
+
+	public static backgroundHex( hex: string ): string {
+		const rgb = convertHexToRGB( hex );
+		return `\x1B[48;2;${ rgb[ 0 ] };${ rgb[ 1 ] };${ rgb[ 2 ] }m`;
+	}
+
+	public static backgroundAnsi256( color: number ): string {
+		return `\x1B[48;5;${ color }m`;
+	}
+
+	public static backgroundName( name: ANSI_Background_T ): string {
+		const result = backgroundColors[ name ];
+		if ( result !== undefined ) {
+			return result;
+		}
+
+		return backgroundColors.default;
 	}
 
 	public static fontStyle( style: ANSI_Style_T ): string {
