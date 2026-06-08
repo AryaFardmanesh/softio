0322017d27c0d00b55133ef261536d53213a1b4d
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2026-06-08 15:45:53 +0330

    Fix: method name.

--- src/output.ts
+++ src/output.ts
@@ -45,7 +45,7 @@ export default class Out {
 			// Retrieve the styles
 			Attr.background( Attr[ _bg ] );
 			Attr.color( Attr[ _color ] );
-			if ( style?.style ) Attr.styleReset( style.style );
+			if ( style?.style ) Attr.fontStyleReset( style.style );
 
 			return result;
 		} ) as T;
